ec3d7ea7f22d60a8b4987262134d2b1f9abc0f9f
Phillip Leipert <phillip_leipert@web.de>
2026-02-01 19:14:32 +0100

    resolve CORS - hopefully

--- strapi-website/config/middlewares.ts
+++ strapi-website/config/middlewares.ts
@@ -1,4 +1,16 @@
-export default [
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://stuk.dev', 'http://localhost:3000'],
+      headers: '*',
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      credentials: true,
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
